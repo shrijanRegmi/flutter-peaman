239f4f02b00146cae5dec9b215b057fe67d4a9fe
Shrijan Regmi <ilyyhs9@gmail.com>
2020-07-31 14:43:29 +0545

    displaying items in chat list improved

--- lib/services/database_services/message_provider.dart
+++ lib/services/database_services/message_provider.dart
@@ -7,8 +7,8 @@ import 'package:peaman/models/app_models/message_model.dart';
 class MessageProvider {
   final String chatId;
   final DocumentReference messageRef;
-  final DocumentReference appUserRef;
-  MessageProvider({this.chatId, this.messageRef, this.appUserRef});
+  final String appUserId;
+  MessageProvider({this.chatId, this.messageRef, this.appUserId});
 
   final _ref = Firestore.instance;
 
@@ -53,6 +53,11 @@ class MessageProvider {
       DocumentReference _firstUserRef;
       DocumentReference _secondUserRef;
 
+      final _users = [myId, friendId];
+      Map<String, dynamic> _userData = {
+        'users': _users,
+      };
+
       if (_isAppUserFirstUser) {
         _firstUserRef = _ref.collection('users').document(myId);
         _secondUserRef = _ref.collection('users').document(friendId);
@@ -60,7 +65,7 @@ class MessageProvider {
         _firstUserRef = _ref.collection('users').document(friendId);
         _secondUserRef = _ref.collection('users').document(myId);
       }
-
+      await _chatRef.updateData(_userData);
       await _chatRef.updateData({'first_user_ref': _firstUserRef});
       await _chatRef.updateData({'second_user_ref': _secondUserRef});
 
@@ -105,8 +110,8 @@ class MessageProvider {
   Stream<List<Chat>> get chatList {
     return _ref
         .collection('chats')
-        .where('first_user_ref', isEqualTo: appUserRef)
-        .orderBy('last_updated', descending: true)
+        .where('users', arrayContains: appUserId)
+        // .orderBy('last_updated', descending: true)
         .snapshots()
         .map(_chatsFromFirebase);
   }
