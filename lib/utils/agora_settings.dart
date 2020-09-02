f3c5094184a6337eea5a57d475dba0e6178789a1
Shrijan <ilyyhs9@gmail.com>
2020-09-02 20:12:52 +0545

    chat functionality added

---
+++ lib/utils/agora_settings.dart
@@ -0,0 +1,2 @@
+// Agora AppId
+const APP_ID = 'e3b063e40fbe45ddbf0def7e720e0a59';
--- lib/views/screens/chat_convo_screen.dart
+++ lib/views/screens/chat_convo_screen.dart
@@ -67,7 +67,6 @@ class _ChatConvoScreenState extends State<ChatConvoScreen> {
         }
 
         if (_unreadMessagesCount != 0) {
-          print('Called');
           vm.updateChatData(_data, widget.chat?.id);
         }
 
